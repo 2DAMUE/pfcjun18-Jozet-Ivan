281c7628193ef1dbc9e9b414aac85a815b4cdc5b
Ivan Zhen <ivangpajon@hotmail.com>
2018-06-03 23:39:32 +0200

    Se ha añadido un icono svg

--- app/src/main/java/com/quadram/futh/DeviceFragment.java
+++ app/src/main/java/com/quadram/futh/DeviceFragment.java
@@ -4,6 +4,7 @@ package com.quadram.futh;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -91,7 +92,7 @@ public class DeviceFragment extends Fragment {
     }
 
     private void onClickGas(View v) {
-
+        Log.d("CARDVIEW", "Dentro del cardview onClick");
     }
 
     private void recogerDatosFirebase() {
